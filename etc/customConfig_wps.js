86811f69ce28712022053bf219b5abea688100f2
dallery <dallery.donatien@gmail.com>
2018-07-02 14:49:49 +0200

    Service xyOnNetwork

--- etc/customConfig_wps.js
+++ etc/customConfig_wps.js
@@ -196,13 +196,14 @@ customConfig = {
         idGetStation: "getStations",
         idGetTransfr: "getTransfr",
         idModel: "transfr",
-        //datainputs: "X=/;Y=/;Start=/;End=/;Name=/;DeltaT=/;InBasin=",
+        idXyOnNetwork: "xyOnNetwork",
 	    datainputs: "X/Y/Start/End/Name/DeltaT/InBasin/ListStations",
         storeExecuteResponse: true,
         lineage: true,
         status: true,
         describeWPS: 'http://wps.geosas.fr/simfen?service=WPS&version=1.0.0&request=describeProcess&identifier=transfr',
-        refreshTime: 3000
+        refreshTime: 5000,
+        refreshTimeXY: 1000
     },
 
     /**
